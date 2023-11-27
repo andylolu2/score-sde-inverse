33507088e1476a29425bc81a85577ecf31987dbd
Andy Lo <andylolu24@gmail.com>
2023-11-27 21:27:25 +0000

    Tuning bayes opt procedure

--- scripts/optimise_lambda.py
+++ scripts/optimise_lambda.py
@@ -1,12 +1,13 @@
-from absl import app, flags
-from bayes_opt import BayesianOptimization
-import pandas as pd
-
-import torch
-
-import sys
+import math
 import os
-sys.path.append(os.getcwd())
+import sys
+from pathlib import Path
+
+import pandas as pd
+import torch
+from absl import app, flags
+from bayes_opt import BayesianOptimization, UtilityFunction
+from sklearn.gaussian_process.kernels import RBF
 
 from scripts.shared_utils import SharedUtils, compute_psnr
 
@@ -15,62 +16,79 @@ flags.DEFINE_enum("dataset", "cifar10", ["cifar10", "celeba"], "Dataset to use."
 flags.DEFINE_integer("num_scales", 100, "Number of discretisation steps")
 flags.DEFINE_integer("batch_size", 1, "Batch size")
 flags.DEFINE_integer("num_batches", 1, "Number of samples to generate")
-flags.DEFINE_integer("samples_per_image", 1, "Number of reconstructed samples per image")
-flags.DEFINE_integer("iterations", 10, "Number of Bayesian optimization iterations to perform")
-flags.DEFINE_integer("init_points", 2, "Number of random Bayesian optimization initialization points")
-flags.DEFINE_enum("task", "denoise", ["deblur_gaussian", "denoise"], "Inverse task to use")
+flags.DEFINE_integer(
+    "samples_per_image", 1, "Number of reconstructed samples per image"
+)
+flags.DEFINE_integer(
+    "iterations", 10, "Number of Bayesian optimization iterations to perform"
+)
+flags.DEFINE_integer(
+    "init_points", 2, "Number of random Bayesian optimization initialization points"
+)
+flags.DEFINE_enum(
+    "task", "denoise", ["deblur_gaussian", "denoise"], "Inverse task to use"
+)
+flags.DEFINE_string("save_dir", None, "Directory to save samples", required=True)
+
+sys.path.append(os.getcwd())
 
 iteration_counter = 0
 results = []
 
+
 def main(_):
     global iteration_counter
+    save_dir = Path(FLAGS.save_dir)
+    save_dir.mkdir(parents=True, exist_ok=True)
 
     utils = SharedUtils(FLAGS, train=True)
 
-    pbounds = {'lambda_value': (0.0, 1.0)}
-
     optimizer = BayesianOptimization(
-        f=lambda lambda_value: objective_function(utils, lambda_value, FLAGS.num_batches),
-        pbounds=pbounds,
+        f=lambda log_lambda: objective_function(utils, log_lambda),
+        pbounds={"log_lambda": (-3, 0)},
         random_state=1,
+        allow_duplicate_points=True,
     )
-
+    optimizer.set_gp_params(kernel=RBF(length_scale=1), alpha=0.1, normalize_y=True)
     optimizer.maximize(
         init_points=FLAGS.init_points,
-        n_iter=FLAGS.init_points,
+        n_iter=FLAGS.iterations,
+        acquisition_function=UtilityFunction("ei"),
     )
 
-    best_lambda = optimizer.max['params']['lambda_value']
-    print("Best lambda value:", best_lambda)
+    best_log_lambda = optimizer.max["params"]["log_lambda"]
+    best_lambda = math.pow(10, best_log_lambda)
+    print(f"Best lambda value: {best_lambda}")
     print(f"Total iterations: {iteration_counter}")
 
     # Convert results to a DataFrame
     df = pd.DataFrame(results)
-    df.to_csv('optimization_results_2.csv', index=False)
+    df.to_csv(save_dir / "optimization_results.csv", index=False)
 
 
-def objective_function(utils, lambda_value, num_batches):
+def objective_function(utils, log_lambda):
     global iteration_counter
+    lambda_ = math.pow(10, log_lambda)
     iteration_counter += 1
-    print(f"Running iteration {iteration_counter} with lambda value {lambda_value}")
-    sampling_fn = utils.get_sampling_fn(lambda_value)
-    dataset = torch.stack([utils.dataset[ind] for ind in range(utils.config.eval.batch_size*num_batches)])
+    sampling_fn = utils.get_sampling_fn(lambda_)
 
+    dataset = torch.stack(
+        [utils.dataset[i] for i in range(FLAGS.batch_size * FLAGS.num_batches)]
+    )
     samples = []
 
-    for i in range(num_batches):
-        print('Sampling batch', i)
-        y = utils.inverse_task.forward(dataset[i*utils.config.eval.batch_size:(i+1)*utils.config.eval.batch_size].to(utils.config.device))
-        sample, _ = sampling_fn(utils.score_model, y)
-        samples.append(sample.detach().cpu())
+    for i in range(FLAGS.num_batches):
+        x = dataset[i * FLAGS.batch_size : (i + 1) * FLAGS.batch_size].to(
+            utils.config.device
+        )
+        y = utils.inverse_task.forward(x)
+        x_hat, _ = sampling_fn(utils.score_model, y)
+        samples.append(x_hat.detach().cpu())
 
     samples = torch.cat(samples)
-
     psnr = compute_psnr(dataset, samples)
-    torch.cuda.empty_cache()
 
-    results.append({'iteration': iteration_counter, 'lambda': lambda_value, 'psnr': psnr})
+    results.append({"iteration": iteration_counter, "lambda": lambda_, "psnr": psnr})
     return psnr
 
 
--- scripts/shared_utils.py
+++ scripts/shared_utils.py
@@ -1,19 +1,20 @@
-import torch
 import numpy as np
-from PIL import Image
+import torch
 from absl import logging
+from PIL import Image
+from torch.utils.data import random_split
 from torchmetrics.image import PeakSignalNoiseRatio
-from score_inverse.models.utils import create_model
-from score_inverse.models.ema import ExponentialMovingAverage
-from score_inverse.tasks.deblur import DeblurTask
-from score_inverse.sde import get_sde
+
+from configs.ve.celebahq_256_ncsnpp_continuous import get_config as get_celeba_config
+from configs.ve.cifar10_ncsnpp_deep_continuous import get_config as get_cifar10_config
+from score_inverse.datasets import CIFAR10, CelebA
 from score_inverse.datasets.scalers import get_data_inverse_scaler
+from score_inverse.models.ema import ExponentialMovingAverage
+from score_inverse.models.utils import create_model
 from score_inverse.sampling import get_corrector, get_predictor
 from score_inverse.sampling.inverse import get_pc_inverse_solver
-from configs.ve.cifar10_ncsnpp_deep_continuous import get_config as get_cifar10_config
-from configs.ve.celebahq_256_ncsnpp_continuous import get_config as get_celeba_config
-from score_inverse.datasets import CelebA, CIFAR10
-
+from score_inverse.sde import get_sde
+from score_inverse.tasks.deblur import DeblurTask
 from score_inverse.tasks.denoise import DenoiseTask
 
 
@@ -25,27 +26,32 @@ def tensor_to_image(x: torch.Tensor) -> Image.Image:
 
 
 def compute_psnr(source, reconstructed):
-    psnr = PeakSignalNoiseRatio(data_range=(0, 1))
+    psnr = PeakSignalNoiseRatio(data_range=(0, 1)).to(source.device)
     return psnr(reconstructed, source).cpu().numpy()
 
 
 class SharedUtils:
-
     def __init__(self, FLAGS, train=True):
         if FLAGS.dataset == "cifar10":
             self.config = get_cifar10_config()
-            self.ckpt_path = "checkpoints/ve/cifar10_ncsnpp_deep_continuous/checkpoint_12.pth"
+            self.ckpt_path = (
+                "checkpoints/ve/cifar10_ncsnpp_deep_continuous/checkpoint_12.pth"
+            )
             self.dataset = CIFAR10(train=train)
         elif FLAGS.dataset == "celeba":
             self.config = get_celeba_config()
-            self.ckpt_path = "checkpoints/ve/celebahq_256_ncsnpp_continuous/checkpoint_48.pth"
+            self.ckpt_path = (
+                "checkpoints/ve/celebahq_256_ncsnpp_continuous/checkpoint_48.pth"
+            )
             self.dataset = CelebA(train=train)
         else:
             raise ValueError(f"Unknown dataset {FLAGS.dataset}")
 
         self.config.model.num_scales = FLAGS.num_scales
         self.config.eval.batch_size = FLAGS.batch_size
-        self.data_loader = self.get_dataloader(FLAGS.samples_per_image, FLAGS.batch_size)
+        self.data_loader = self.get_dataloader(
+            FLAGS.samples_per_image, FLAGS.batch_size
+        )
         self.score_model = self.load_checkpoint()
         self.inverse_task = self.get_inverse_task(FLAGS.task)
 
@@ -63,9 +69,9 @@ class SharedUtils:
 
     def get_inverse_task(self, task_name: str):
         if task_name == "deblur_gaussian":
-            return DeblurTask(self.dataset.img_size, kernel_type="gaussian", kernel_size=5).to(
-                device=self.config.device
-            )
+            return DeblurTask(
+                self.dataset.img_size, kernel_type="gaussian", kernel_size=5
+            ).to(device=self.config.device)
         elif task_name == "denoise":
             return DenoiseTask(self.dataset.img_size).to(device=self.config.device)
         else:
